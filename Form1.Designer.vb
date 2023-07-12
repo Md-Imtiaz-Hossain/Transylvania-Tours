92600f372913f998facd3ddd42457bff021b42a3
Md Imtiaz Hossain <imtiaz005123@gmail.com>
2023-07-12 09:44:25 +0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,357 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        GroupBox1 = New GroupBox()
+        RadioButton8 = New RadioButton()
+        RadioButton7 = New RadioButton()
+        RadioButton6 = New RadioButton()
+        RadioButton5 = New RadioButton()
+        GroupBox2 = New GroupBox()
+        CheckBox3 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox1 = New CheckBox()
+        GroupBox3 = New GroupBox()
+        RadioButton4 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        GroupBox4 = New GroupBox()
+        RadioButton14 = New RadioButton()
+        RadioButton15 = New RadioButton()
+        RadioButton16 = New RadioButton()
+        Button2 = New Button()
+        Button3 = New Button()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        GroupBox4.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = Color.Transparent
+        GroupBox1.Controls.Add(RadioButton8)
+        GroupBox1.Controls.Add(RadioButton7)
+        GroupBox1.Controls.Add(RadioButton6)
+        GroupBox1.Controls.Add(RadioButton5)
+        GroupBox1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        GroupBox1.Location = New Point(302, 195)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(232, 140)
+        GroupBox1.TabIndex = 24
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Lunch"
+        ' 
+        ' RadioButton8
+        ' 
+        RadioButton8.AutoSize = True
+        RadioButton8.FlatStyle = FlatStyle.System
+        RadioButton8.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton8.Location = New Point(27, 103)
+        RadioButton8.Name = "RadioButton8"
+        RadioButton8.Size = New Size(164, 24)
+        RadioButton8.TabIndex = 15
+        RadioButton8.TabStop = True
+        RadioButton8.Text = "Baked potato £4.15"
+        RadioButton8.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton7
+        ' 
+        RadioButton7.AutoSize = True
+        RadioButton7.FlatStyle = FlatStyle.System
+        RadioButton7.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton7.Location = New Point(27, 78)
+        RadioButton7.Name = "RadioButton7"
+        RadioButton7.Size = New Size(107, 24)
+        RadioButton7.TabIndex = 14
+        RadioButton7.TabStop = True
+        RadioButton7.Text = "Soup £2.75"
+        RadioButton7.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton6
+        ' 
+        RadioButton6.AutoSize = True
+        RadioButton6.FlatStyle = FlatStyle.System
+        RadioButton6.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton6.Location = New Point(27, 53)
+        RadioButton6.Name = "RadioButton6"
+        RadioButton6.Size = New Size(136, 24)
+        RadioButton6.TabIndex = 13
+        RadioButton6.TabStop = True
+        RadioButton6.Text = "Sandwich £3.50"
+        RadioButton6.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton5
+        ' 
+        RadioButton5.AutoSize = True
+        RadioButton5.FlatStyle = FlatStyle.System
+        RadioButton5.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton5.Location = New Point(27, 28)
+        RadioButton5.Name = "RadioButton5"
+        RadioButton5.Size = New Size(110, 24)
+        RadioButton5.TabIndex = 12
+        RadioButton5.TabStop = True
+        RadioButton5.Text = "Salad £5.00"
+        RadioButton5.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.BackColor = Color.Transparent
+        GroupBox2.Controls.Add(CheckBox3)
+        GroupBox2.Controls.Add(CheckBox2)
+        GroupBox2.Controls.Add(CheckBox1)
+        GroupBox2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        GroupBox2.ForeColor = SystemColors.Desktop
+        GroupBox2.Location = New Point(302, 341)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(232, 118)
+        GroupBox2.TabIndex = 25
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Extra Activities : "
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.FlatStyle = FlatStyle.System
+        CheckBox3.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        CheckBox3.Location = New Point(27, 77)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(191, 24)
+        CheckBox3.TabIndex = 26
+        CheckBox3.Text = "Bungee jumping £30.00"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.FlatStyle = FlatStyle.System
+        CheckBox2.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        CheckBox2.Location = New Point(27, 52)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(140, 24)
+        CheckBox2.TabIndex = 25
+        CheckBox2.Text = "Falconry £25.00"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.FlatStyle = FlatStyle.System
+        CheckBox1.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        CheckBox1.Location = New Point(27, 27)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(136, 24)
+        CheckBox1.TabIndex = 24
+        CheckBox1.Text = "Archery £20.00"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.BackColor = Color.Transparent
+        GroupBox3.Controls.Add(RadioButton4)
+        GroupBox3.Controls.Add(RadioButton3)
+        GroupBox3.Controls.Add(RadioButton2)
+        GroupBox3.Controls.Add(RadioButton1)
+        GroupBox3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        GroupBox3.Location = New Point(54, 195)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(232, 140)
+        GroupBox3.TabIndex = 25
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Date of visit : "
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.FlatStyle = FlatStyle.System
+        RadioButton4.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton4.Location = New Point(35, 103)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(176, 24)
+        RadioButton4.TabIndex = 10
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "Saturday 29th August"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.FlatStyle = FlatStyle.System
+        RadioButton3.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton3.Location = New Point(35, 78)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(180, 24)
+        RadioButton3.TabIndex = 9
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Saturday 22nd August"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.FlatStyle = FlatStyle.System
+        RadioButton2.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton2.Location = New Point(35, 53)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(176, 24)
+        RadioButton2.TabIndex = 8
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Saturday 15th August"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.FlatStyle = FlatStyle.System
+        RadioButton1.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton1.Location = New Point(35, 28)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(168, 24)
+        RadioButton1.TabIndex = 7
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Saturday 8th August"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox4
+        ' 
+        GroupBox4.BackColor = Color.Transparent
+        GroupBox4.Controls.Add(RadioButton14)
+        GroupBox4.Controls.Add(RadioButton15)
+        GroupBox4.Controls.Add(RadioButton16)
+        GroupBox4.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        GroupBox4.ForeColor = SystemColors.Desktop
+        GroupBox4.Location = New Point(54, 341)
+        GroupBox4.Name = "GroupBox4"
+        GroupBox4.Size = New Size(232, 118)
+        GroupBox4.TabIndex = 27
+        GroupBox4.TabStop = False
+        GroupBox4.Text = "Activities : "
+        ' 
+        ' RadioButton14
+        ' 
+        RadioButton14.AutoSize = True
+        RadioButton14.FlatStyle = FlatStyle.System
+        RadioButton14.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton14.Location = New Point(35, 78)
+        RadioButton14.Name = "RadioButton14"
+        RadioButton14.Size = New Size(153, 25)
+        RadioButton14.TabIndex = 18
+        RadioButton14.TabStop = True
+        RadioButton14.Text = "Skydiving £30.00"
+        RadioButton14.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton15
+        ' 
+        RadioButton15.AutoSize = True
+        RadioButton15.FlatStyle = FlatStyle.System
+        RadioButton15.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton15.Location = New Point(35, 53)
+        RadioButton15.Name = "RadioButton15"
+        RadioButton15.Size = New Size(167, 25)
+        RadioButton15.TabIndex = 17
+        RadioButton15.TabStop = True
+        RadioButton15.Text = "Hill walking £15.00"
+        RadioButton15.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton16
+        ' 
+        RadioButton16.AutoSize = True
+        RadioButton16.FlatStyle = FlatStyle.System
+        RadioButton16.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        RadioButton16.Location = New Point(35, 28)
+        RadioButton16.Name = "RadioButton16"
+        RadioButton16.Size = New Size(172, 25)
+        RadioButton16.TabIndex = 16
+        RadioButton16.TabStop = True
+        RadioButton16.Text = "Horse riding £25.00"
+        RadioButton16.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Orange
+        Button2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.Location = New Point(302, 474)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(104, 38)
+        Button2.TabIndex = 29
+        Button2.Text = "Next"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Orange
+        Button3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Button3.Location = New Point(182, 474)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(104, 38)
+        Button3.TabIndex = 30
+        Button3.Text = "Total Cost"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(1093, 619)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(GroupBox4)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        GroupBox4.ResumeLayout(False)
+        GroupBox4.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton8 As RadioButton
+    Friend WithEvents RadioButton7 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents RadioButton14 As RadioButton
+    Friend WithEvents RadioButton15 As RadioButton
+    Friend WithEvents RadioButton16 As RadioButton
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
